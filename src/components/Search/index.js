d48023f2e89e47fbabdc945be762f4eb8e1da02c
Wallace Delesposti <wallace.delesposti@gmail.com>
2022-05-15 03:16:22 -0300

    feat: create hit component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
 
+import Hit from "./Hit";
 import * as S from "./styled";
 
 const algolia = {
@@ -12,8 +13,6 @@ const algolia = {
 }
 
 const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
-
-
 
 const Search = () => (
   <S.SearchWrapper>
@@ -25,7 +24,7 @@ const Search = () => (
         },
       }}
       />
-      <Hits />
+      <Hits hitComponent={Hit}/>
     </InstantSearch>
   </S.SearchWrapper>
 );
